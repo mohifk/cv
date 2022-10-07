a8e4e66a65851e6ddf0092f278816c6fbb7f2173
mohifk <mohi.fk@hotmail.com>
2022-10-07 13:41:31 +0330

    edit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-import requests
-import os
+
+
 def home_page_view(request):
     return render(request ,'.\cv-temp\index.html')
 def language(request):
@@ -13,20 +13,6 @@ def projects(request):
     return render(request ,'.\cv-temp\projects.html')
 def contects_bio(request):
     return render(request ,'.\cv-temp\contects_bio.html')
+
+
 # Create your views here.
-def index(request):
-    r = requests.get('https://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
-def index(request):
-    times = int(os.environ.get('TIMES', 3))
-    return HttpResponse('Hello! ' * times)
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
-
