2a32d77532efb57e97568fdf7ed4898765a6376a
mohifk <mohi.fk@hotmail.com>
2022-11-02 21:01:26 +0330

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render
 
 
 def home_page_view(request):
-    return render(request ,'.\cv-temp\index.html')
+    return render(request ,'cv-temp/index.html')
 def language(request):
-    return render(request ,'.\cv-temp\language.html')
+    return render(request ,'cv-temp/language.html')
 def education(request):
-    return render(request ,'.\cv-temp\education.html')
+    return render(request ,'cv-temp/education.html')
 def abilites(request):
-    return render(request ,'.\cv-temp\\abilites.html')
+    return render(request ,'cv-temp/abilites.html')
 def projects(request):
-    return render(request ,'.\cv-temp\projects.html')
+    return render(request ,'cv-temp/projects.html')
 def contects_bio(request):
-    return render(request ,'.\cv-temp\contects_bio.html')
+    return render(request ,'cv-temp/contects_bio.html')
 
 
 # Create your views here.
